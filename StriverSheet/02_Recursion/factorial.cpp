081dff03b3c3bee6ae2259fae20c431a32eb9c11
Ayush <ayushkumar85385@gmail.com>
2025-09-23 11:11:37 +0530

    solved some striver questions

---
+++ StriverSheet/02_Recursion/factorial.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n) {
+  if (n < 1) return 1;
+  else return fact(n-1) * n;
+}
+
+int main() {
+  int fac = fact(5);
+  cout << fac;
+  return 0;
+}

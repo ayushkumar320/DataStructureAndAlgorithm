526cbfd8abb20189578c968471e470c202ed5148
Ayush <ayushkumar85385@gmail.com>
2025-11-01 12:43:49 +0530

    added betetr complexity approach

--- GeeksForGeeks/01_arrays/01_duplicate.cpp
+++ GeeksForGeeks/01_arrays/01_duplicate.cpp
@@ -15,10 +15,19 @@ bool checkDuplicate(vector<int> &nums, int k) {
 }
 
 // Betetr complexity using hash maps
-
-bool checkDuplicateHash(vector<int> &nums, int k) {
-  unordered_map<int, int> hash;`
-}
+bool checkDuplicateOptimized(vector<int> &nums, int k) {
+  unordered_map<int, int> indexMap; // Map to store the last index of each element
+  for(int i = 0; i < nums.size(); i++) {
+    if(indexMap.find(nums[i]) != indexMap.end()) {
+      // Element found in map, check the index difference
+      if(i - indexMap[nums[i]] <= k) {
+        return true;
+      }
+    }
+    // Update the last index of the element
+    indexMap[nums[i]] = i;
+  }
+  return false;
 
 int main() {
   vector<int> nums = {1, 2, 3, 1, 2, 3};

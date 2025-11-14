ca1adbfdf76adcb22a1494ba38635036d014cb6e
Ayush <ayushkumar85385@gmail.com>
2025-11-14 07:19:29 +0530

    added mergesort

--- StriverSheet/04_Sorting/04_mergeSort.cpp
+++ StriverSheet/04_Sorting/04_mergeSort.cpp
@@ -2,13 +2,30 @@
 using namespace std;
 
 void merge(vector<int> &nums, int low, int mid, int high) {
+  vector<int> temp;
   int left = low;
-  int right = high;
-  while(low < mid && right < high) {
-    
+  int right = mid+1;
+  while(left <= mid && right <= high) {
+    if(nums[left] < nums[right]) {
+      temp.push_back(nums[left]);
+      left++;
+    } else {
+      temp.push_back(nums[right]);
+      right++;
+    }
+  }
+  while(left <= mid) {
+    temp.push_back(nums[left]);
+    left++;
+  }
+  while(right <= high) {
+    temp.push_back(nums[right]);
+    right++;
+  }
+  for (int i = low; i <= high; i++) {
+    nums[i] = temp[i - low];
   }
 }
-
 
 void mergeSort(vector<int> &nums, int low, int high) {
   if(low >= high) return;
@@ -20,5 +37,12 @@ void mergeSort(vector<int> &nums, int low, int high) {
 
 
 int main() {
+  vector<int> nums = {1, 11, 14, 2, 5, 7, 23, 0, 6, 8};
+  int high = nums.size() - 1;
+  mergeSort(nums, 0, high);
+  for (auto it : nums) {
+    cout << it << " ";
+  }
+  return 0;
   return 0;
 }
